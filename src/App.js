9ee7a6d51947ef91eed87bb44e67811a6b5c2f16
GER-ITL <danteryzvelt@inbox.ru>
2023-11-01 18:15:19 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
 import CostItem from "./components/CostItem/CostItem";
 import './styles/App.scss'
-const costs = [
-  {
-      data: new Date(),
-      description: 'Phone',
-      amount: '$ 999.99',
-  },
-  {
-      data: new Date(),
-      description: 'MacBook',
-      amount: '$ 1209.99',
-  },
-  {
-      data: new Date(),
-      description: 'TV',
-      amount: '$ 500.00',
-  },
-]
+
 function App() {
   return (
     <>
-    <CostItem 
-    data = {costs[0].data}
-    description = {costs[0].description}
-    amount = {costs[0].amount}
-    />
-    <CostItem 
-    data = {costs[1].data}
-    description = {costs[1].description}
-    amount = {costs[1].amount}
-    />
-    <CostItem 
-    data = {costs[2].data}
-    description = {costs[2].description}
-    amount = {costs[2].amount}
-    />
+      <CostItem/>
     </>
   );
 }
--- src/components/CostItem/CostItem.jsx
+++ src/components/CostItem/CostItem.jsx
@@ -1,17 +1,45 @@
 import React from 'react'
 import './CostItem.scss'
-
-const CostItem = (props) => {
+const costs = [
+  {
+      id:1,
+      data: new Date(),
+      description: 'Phone',
+      amount: '$ 999.99',
+  },
+  {
+      id:2,
+      data: new Date(),
+      description: 'MacBook',
+      amount: '$ 1209.99',
+  },
+  {
+      id:3,
+      data: new Date(),
+      description: 'TV',
+      amount: '$ 500.00',
+  },
+  
+]
+const CostItem = () => {
 
   return (
-    <div className='cost-item'>
-      <div className='cost-data'><label htmlFor="data">{props.data.toDateString()}</label></div>
-      <div className='cost-info'>
-      <label className='cost-price'htmlFor="price">{props.amount}</label>
-      <label  htmlFor="element">{props.description}</label>
-      </div>
+    <>
+      {costs.map((cost)=>{
+        return(
+          <div className='cost-item' key={cost.id}>
+          <div className='cost-data'><label htmlFor="data">{cost.data.toDateString()}</label></div>
+          <div className='cost-info'>
+          <label className='cost-price'htmlFor="price">{cost.amount}</label>
+          <label  htmlFor="element">{cost.description}</label>
+          </div>
+          <button>Change cost</button>
+        </div>
+        )
+      })}
 
-    </div>
+    </>
+
   )
 }
 
